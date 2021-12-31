df9f93e8a1b3eb878be2728e63f0a337645aaac1
Sensol2 <hhncn4471@soongsil.ac.kr>
2021-12-31 21:31:35 +0900

    Post model Added

--- backend/post/models.py
+++ backend/post/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
+    finished_at = models.DateTimeField(null=True)                      #완료일
+    created_at = models.DateTimeField(default=timezone.now)   #만들어진 날짜
+    success = models.BooleanField(default=False)
 
     def __str__(self):
         """A string representation of the model."""
