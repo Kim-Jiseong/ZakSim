552c48ae6b837672862855052ad8507deb0df18a
Sensol2 <hhncn4471@soongsil.ac.kr>
2021-12-31 22:54:16 +0900

    Post added

--- backend/post/models.py
+++ backend/post/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
-    finished_at = models.DateTimeField(null=True)                      #완료일
+    finished_at = models.DateField(null=True)                      #완료일
     created_at = models.DateTimeField(default=timezone.now)   #만들어진 날짜
     success = models.BooleanField(default=False)
 
--- backend/post/serializers.py
+++ backend/post/serializers.py
@@ -4,7 +4,11 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         fields = (
             'id',
+            'author',
             'title',
             'content',
+            'finished_at',
+            'created_at',
+            'success'
         )
         model = Post
